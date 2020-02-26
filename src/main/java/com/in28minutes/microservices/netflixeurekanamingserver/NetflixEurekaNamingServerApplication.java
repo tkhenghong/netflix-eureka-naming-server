49dadbd1e3222f3c9effca9ef30a2b0ea31b4e7b
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-02-26 08:10:34 +0800

    Initialize Eureka naming server

--- src/main/java/com/in28minutes/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ src/main/java/com/in28minutes/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// You can type the following link directly
+// http://localhost:8761/
 @SpringBootApplication
 @EnableEurekaServer
 public class NetflixEurekaNamingServerApplication {
